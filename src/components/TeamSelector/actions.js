8bbaaab4feeddad4af947a218dff5640e12fcd49
Luigi Poole <luigipoole@outlook.com>
2015-12-23 13:47:38 -0800

    get oauth working

--- src/components/TeamSelector/actions.js
+++ src/components/TeamSelector/actions.js
@@ -3,17 +3,19 @@ import alt from '../../alt';
 import OAuthUtil from '../../utils/OAuthUtil';
 
 class TeamselectorActions {
-    constructor() {
-        this.generateActions(
-        );
-    }
+	constructor() {
+		this.generateActions(
+			'added'
+		);
+	}
 
-    add(){
-        this.dispatch();
+	add() {
+		this.dispatch();
 
-        OAuthUtil.getAuthorization();
+		OAuthUtil.getAuthorization()
+			.then(token => this.actions.added(token));
 
-    }
+	}
 
 
 }
--- src/utils/OAuthUtil.js
+++ src/utils/OAuthUtil.js
@@ -1,45 +1,58 @@
 import Promise from 'bluebird';
+import request from 'request';
+import slackAPI from 'slack-api';
 import {
-    remote
+	remote
 }
 from 'electron';
 
 const BrowserWindow = remote.BrowserWindow;
 
 module.exports = {
-    getAuthorization() {
-        var authWindow = new BrowserWindow({
-            'use-content-size': true,
-            center: true,
-            show: false,
-            resizable: true,
-            'always-on-top': false,
-            'standard-window': true,
-            'auto-hide-menu-bar': true,
-            'node-integration': false
-        });
+	getAuthorization() {
+		const authWindow = new BrowserWindow({
+			'use-content-size': true,
+			center: true,
+			show: false,
+			resizable: true,
+			title: 'Slack authorize',
+			'always-on-top': false,
+			'standard-window': true,
+			'auto-hide-menu-bar': true,
+			'node-integration': false
+		});
 
-        var client_id = '8772351907.17263173446';
+		const client_id = '8772351907.17263173446';
 
-        var authUrl = 'https://slack.com/oauth/authorize?client_id=' + client_id + '&scope=client';
+		const authUrl = 'https://slack.com/oauth/authorize?client_id=' + client_id + '&scope=client';
 
-        authWindow.loadUrl(authUrl);
-        authWindow.show();
+		authWindow.loadUrl(authUrl);
+		authWindow.show();
 
-        return new Promise((resolve, reject) => {
-            authWindow.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
-                var raw_code = /code=([^&]*)/.exec(newUrl) || null,
-                    code = (raw_code && raw_code.length > 1) ? raw_code[1] : null,
-                    error = /\?error=(.+)$/.exec(newUrl);
-                if (code || error)
-                    authWindow.close();
-                if (code) {
-                    resolve(code)
-                } else if (error) {
-                    alert("Oops! Something went wrong and we couldn't log into slack. Please try again.");
-                    reject();
-                }
-            });
-        });
-    },
+		authWindow.webContents.on('did-finish-load', () => authWindow.focus());
+
+		return new Promise((resolve, reject) => {
+
+			authWindow.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
+				var raw_code = /code=([^&]*)/.exec(newUrl) || null,
+					code = (raw_code && raw_code.length > 1) ? raw_code[1] : null,
+					error = /\?error=(.+)$/.exec(newUrl);
+				if (code || error)
+					authWindow.close();
+				if (code) {
+					slackAPI.oauth.access({
+						client_id: client_id,
+						client_secret: 'a30268e78d0d54e930a67cd41333c409',
+						code: code
+					}, (err, token) => {
+						if (err)
+							return reject(err);
+						resolve(token);
+					});
+				} else if (error) {
+					reject();
+				}
+			});
+		});
+	},
 };
