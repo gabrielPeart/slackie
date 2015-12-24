5ad6a28476dac928f64d23ef6d1df14575090302
Luigi Poole <luigipoole@outlook.com>
2015-12-23 18:10:45 -0800

    more

--- src/components/Team/components/Sidebar/index.js
+++ src/components/Team/components/Sidebar/index.js
@@ -35,13 +35,40 @@ default React.createClass({
         var channels = [];
         if (this.state.team && this.state.team.channels)
             _.forEach(this.state.team.channels, (channel, idx) => {
-                channels.push(
-                    <li key={idx} >
-                        #{channel.name}
-                    </li>
-                );
+                if (channel.is_member && !channel.is_archived)
+                    channels.push(
+                        <li key={idx} >
+                            #{channel.name}
+                        </li>
+                    );
             });
         return channels;
+    },
+    getDMS() {
+        var dms = [];
+        if (this.state.team && this.state.team.dms)
+            _.forEach(this.state.team.dms, (dm, idx) => {
+                if (dm.is_open && dm.is_im)
+                    dms.push(
+                        <li key={idx} >
+                            {dm.name}
+                        </li>
+                    );
+            });
+        return dms;
+    },
+    getGroups() {
+        var groups = [];
+        if (this.state.team && this.state.team.groups)
+            _.forEach(this.state.team.groups, (group, idx) => {
+                if (group.is_open && group.is_group && !group.is_archived)
+                    groups.push(
+                        <li key={idx} >
+                            {group.name}
+                        </li>
+                    );
+            });
+        return groups;
     },
     render() {
         console.log(this.state.team)
@@ -54,7 +81,21 @@ default React.createClass({
                         {this.getChannels()}
                     </ul>
                 </If> 
-                
+
+                <h1>Direct Messages</h1>
+
+                <If test={this.state.team}>
+                    <ul>
+                        {this.getDMS()}
+                    </ul>
+                </If> 
+                <h1>Groups</h1>
+
+                <If test={this.state.team}>
+                    <ul>
+                        {this.getGroups()}
+                    </ul>
+                </If> 
             </aside>
         );
     }
