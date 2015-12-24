dbf10f2fb4c6373460785a1c97a4fdd1fe986e96
Luigi Poole <luigipoole@outlook.com>
2015-12-23 21:10:55 -0800

    starred support

--- src/components/Team/components/Sidebar/index.js
+++ src/components/Team/components/Sidebar/index.js
@@ -35,7 +35,8 @@ default React.createClass({
         var channels = [];
         if (this.state.team && this.state.team.channels)
             _.forEach(this.state.team.channels, (channel, idx) => {
-                if (channel.is_member && !channel.is_archived)
+                console.log(channel)
+                if (channel.is_member && !channel.is_archived && !channel.is_starred)
                     channels.push(
                         <li key={idx} >
                             #{channel.name}
@@ -48,7 +49,7 @@ default React.createClass({
         var dms = [];
         if (this.state.team && this.state.team.dms)
             _.forEach(this.state.team.dms, (dm, idx) => {
-                if (dm.is_open && dm.is_im)
+                if (dm.is_open && dm.is_im && !dm.is_starred)
                     dms.push(
                         <li key={idx} >
                             {dm.name}
@@ -61,7 +62,7 @@ default React.createClass({
         var groups = [];
         if (this.state.team && this.state.team.groups)
             _.forEach(this.state.team.groups, (group, idx) => {
-                if (group.is_open && group.is_group && !group.is_archived)
+                if (group.is_open && group.is_group && !group.is_archived && !group.is_starred)
                     groups.push(
                         <li key={idx} >
                             {group.name}
@@ -70,10 +71,48 @@ default React.createClass({
             });
         return groups;
     },
+    getStarred() {
+        var starred = [];
+        if (!this.state.team)
+            return false;
+
+        _.forEach(this.state.team.groups, (group, idx) => {
+            if (group.is_open && group.is_group && !group.is_archived && group.is_starred)
+                starred.push(
+                    <li key={idx} >
+                        {group.name}
+                    </li>
+                );
+        });
+
+        _.forEach(this.state.team.channels, (channel, idx) => {
+           if (channel.is_member && !channel.is_archived && channel.is_starred)
+                starred.push(
+                    <li key={idx} >
+                        #{channel.name}
+                    </li>
+                );
+        });
+
+        return starred;
+    },
     render() {
         console.log(this.state.team)
+
+        var starred = this.getStarred();
+
         return (
             <aside className="sidebar">
+
+                <If test={starred}>
+                <div>
+                    <h1>Starred</h1>
+                    <ul>
+                        {starred}
+                    </ul>
+                </div>
+                </If> 
+
                 <h1>Channels</h1>
                 
                 <If test={this.state.team}>
