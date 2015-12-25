814ca5190819436d29801380e215f8399fcf6d50
Luigi Poole <luigipoole@outlook.com>
2015-12-24 22:38:41 -0800

    scroll bottom

--- src/components/Team/components/Chat/index.js
+++ src/components/Team/components/Chat/index.js
@@ -6,14 +6,35 @@ import ChatActions from './actions';
 
 export
 default React.createClass({
+
+    componentDidMount() {
+        window.addEventListener('resize', this.scrollBottom);
+    },
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.scrollBottom);
+    },
+
+    scrollBottom() {
+        if (!this.refs['messages'])
+            return;
+
+        console.log(this.refs['messages'].scrollHeight, this.refs['messages'].scrollTop)
+
+        this.refs['messages'].scrollTop = this.refs['messages'].scrollHeight;
+
+    },
+
     render() {
+        _.defer(this.scrollBottom);
+
         return (
             <div className="page">
                 <div className="header">
                     <h1>#general</h1>
                 </div>
 
-                <div className="messages">
+                <div ref="messages" className="messages">
                     {this.props.messages}
                 </div>
             
