84f0d4f17221af75c6342e332dba50d7a848f846
Luigi Poole <luigipoole@outlook.com>
2015-12-27 15:38:59 -0800

    better send

--- src/components/Team/components/Chat/components/input.js
+++ src/components/Team/components/Chat/components/input.js
@@ -12,10 +12,16 @@ default React.createClass({
         let channel = this.props.team.getChannelGroupOrDMByID(this.props.channel.id);
         channel.send(this.refs['chat-input'].value);
 
-
+    
+        this.props.team.emit('message', {
+        	text: this.refs['chat-input'].value,
+        	type: 'message',
+        	user: this.props.team.self.id,
+        	channel: this.props.channel.id,
+        	ts: new Date().getTime() / 1000
+        })
 
         this.refs['chat-input'].value = '';
-
     },
     render() {
         return (
