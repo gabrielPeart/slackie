ddcfb8a539caddf9ce0e7be76bb811736b5f0d8d
Luigi Poole <luigipoole@outlook.com>
2015-12-27 02:20:18 -0800

    cleaner more optimized way of emoji'ing

--- src/utils/slack/components/message.js
+++ src/utils/slack/components/message.js
@@ -2,20 +2,15 @@ import React from 'react';
 import querystring from 'querystring';
 import _ from 'lodash';
 import emojione from 'emojione';
+import ReactEmoji from 'react-emoji';
 import slackdown from 'slackdown';
 import moment from 'moment';
-
-
-emojione.imageType = 'svg';
-emojione.imagePathSVG = '../images/emojione/svg/';
 
 
 
 const formatText = text => {
 
     text = slackdown.parse(_.unescape(querystring.unescape(text)));
-
-    text = emojione.shortnameToImage(text);
 
     return text;
 }
@@ -24,11 +19,26 @@ const formatText = text => {
 
 export
 default React.createClass({
+    mixins: [ReactEmoji],
     render() {
+    	var text = this.emojify(formatText(this.props.text), {
+    		emojiType: 'twemoji',
+    		ext: 'svg',
+    		attributes: {className: 'emoji'}
+    	});
+    	text = text? text: [];
+
+
         return (
-        	<div className="msg">
+            <div className="msg">
         		<div className="time">{moment.unix(this.props.ts).format('h:mm')}</div>
-        		<div dangerouslySetInnerHTML={{__html: formatText(this.props.text)}} />
+        		<div>
+        		    {
+                    	text.map((el, idx) => {
+                    	    return React.isValidElement(el) ? el : <span dangerouslySetInnerHTML={{__html: el}} />;
+                    	})
+                	}
+        		</div>
         	</div>
         );
     }
