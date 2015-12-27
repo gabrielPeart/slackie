44bb568721c58eb356375c5a76d43a0dc7929e8e
Luigi Poole <luigipoole@outlook.com>
2015-12-27 15:15:07 -0800

    get sending working

--- src/components/Team/components/Chat/components/input.js
+++ src/components/Team/components/Chat/components/input.js
@@ -3,22 +3,29 @@ import _ from 'lodash';
 
 export
 default React.createClass({
-	handelSend(e){
-		e.preventDefault();
-		
-		let channel = this.props.team.getChannelGroupOrDMByID(this.props.channel.id);
-		console.log(channel)
+    handelSend(e) {
+        e.preventDefault();
 
-	},
-	render() {
-		return (
-			<div className="chat-input">
+        if (this.refs['chat-input'].value.length === 0)
+            return;
+
+        let channel = this.props.team.getChannelGroupOrDMByID(this.props.channel.id);
+        channel.send(this.refs['chat-input'].value);
+
+
+
+        this.refs['chat-input'].value = '';
+
+    },
+    render() {
+        return (
+            <div className="chat-input">
                 <div className="chat-inner">
                 	<form onSubmit={this.handelSend}>
-                		<input/>
+                		<input ref="chat-input"/>
                 	 </form>
                 </div>
             </div>
-		);
-	}
+        );
+    }
 });
