e25e71ce19ab0b660dc9f2d59db13f753d73fde5
Luigi Poole <luigipoole@outlook.com>
2016-01-02 14:33:06 -0800

    image loader for profile

--- src/utils/slack/components/messageHeader.js
+++ src/utils/slack/components/messageHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageLoader from 'react-imageloader';
 import moment from 'moment';
 
 export
@@ -7,7 +8,9 @@ default React.createClass({
     	var profileImage = (this.props.user && this.props.user.profile) ? this.props.user.profile['image_72'] : ((this.props.user && this.props.user.icons) ? this.props.user.icons[Object.keys(this.props.user.icons)[Object.keys(this.props.user.icons).length - 1]] : '');
         return (
             <div className="message">
-                <img src={profileImage} className="profile" />
+            	<ImageLoader
+                	className="profile"
+                	src={profileImage} />
                 <h1>{(this.props.user && this.props.user.name) ? this.props.user.name : 'Undefined'}</h1>
                 <span className="time">{moment.unix(this.props.time).calendar()}</span>
             </div>
