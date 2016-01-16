00833a73e96324c1117f5b67cf143015f1adb5db
Js41637 ftw <js41637@gmail.com>
2016-01-16 22:50:03 +1100

    Fix up inline text color bar

--- src/utils/slack/components/InlineDescription.js
+++ src/utils/slack/components/InlineDescription.js
@@ -26,13 +26,14 @@ default React.createClass({
         if (this.props.image_url) 
            image = <ImageLoader onLoad={this.handelLoaded} className="description-image" src={this.props.image_url} />;
         return (
-            <div style={{borderColor: (this.props.color ? ('#' + this.props.color) : void 0)}} className="inline-description">
+            <div className="inline-description">
+                <div className="color-bar" style={{backgroundColor: (this.props.color ? ('#' + this.props.color) : void 0)}}></div>
                 {thumb}
-                <span className={this.props.thumb_url ? 'thumb' : ''}>
+                <div className={this.props.thumb_url ? 'inline-description-inner thumb thumb' : 'inline-description-inner thumb'}>
                     <h2>{this.props.service_name}</h2>
                     <a className="description-title" href={this.props.title_link}>{this.props.title}</a>
                     <p dangerouslySetInnerHTML={{__html: new messageFormatUtil(_.unescape(this.props.text), this.props.users, false).parsed }}/>
-                </span>
+                </div>
                 {image}
             </div>
         );
