84a7ebf1b3fca91cda7ef0a79bd8fe85861630a2
Luigi Poole <luigipoole@outlook.com>
2016-01-17 22:37:18 -0800

    fix jenkins

--- util/jenkins.sh
+++ util/jenkins.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 npm prune && npm install --no-optional;
-grunt clean:all && mkdir release && chmod +x util/*/**.sh;
+grunt clean:all;
+mmkdir release && chmod -R 755 util;
 
+util/linux.sh
 util/win32.sh
