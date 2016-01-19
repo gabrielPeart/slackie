b5e3752c42562156e9c13d3b92ac04370220a1e6
Luigi Poole <luigipoole@outlook.com>
2016-01-18 17:22:37 -0800

    uhrgh *stabs*

--- src/utils/slack/components/messageHeader.js
+++ src/utils/slack/components/messageHeader.js
@@ -10,18 +10,14 @@ default React.createClass({
     },
 
     render() {
-    	var profileImage = (this.props.user && this.props.user.profile) ? 
-            this.props.user.profile.image_72 :
-            (this.props.icons && this.props.icons.image_64) ?
-                this.props.icons.image_64 :
-                'null';
 
+    	const profileImage = (this.props.user && this.props.user.profile) ? this.props.user.profile['image_72'] : ((this.props.user && this.props.user.icons) ? this.props.user.icons[Object.keys(this.props.user.icons)[Object.keys(this.props.user.icons).length - 1]] : '')
 
         return (
             <div className="message-header" onClick={this.handelClick}>
             	<ImageLoader
                 	className="profile"
-                	src={profileImage} />
+                	src={profileImage ? profileImage : void 0} />
                 <h1>{(this.props.user && this.props.user.name) ? this.props.user.name : this.props.username ? this.props.username : 'Undefined'}</h1>
                 <span className="time">{moment.unix(this.props.time).calendar()}</span>
             </div>
